7825427b5fd1bc2afa374b1bb31c29985e6992f2
sanjay99-sunny <121281642+sanjay99-sunny@users.noreply.github.com>
2022-12-23 15:30:33 +0530

    Add files via upload

---
+++ jav.js
@@ -0,0 +1,17 @@
+let a=10;
+let b=20;
+c=a+b;
+
+
+let alien = {
+    name:"sanjay",
+    age:50,
+    idd:c,
+}
+    console.log(alien.idd);
+    {
+        let person={
+            name:"sanjay",
+            name1:"kumar",
+        }
+        console.log(person.name+person.name1);    }
